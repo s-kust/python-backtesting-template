8f3ad78e42396fd61468408486d60188b60afc71
SergioKushchenko <serhii.kushchenko@appvales.com>
2024-10-25 10:43:52 +0200

    _check_imported_data - renamed and improved

--- utils/import_data.py
+++ utils/import_data.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 import pandas as pd
 import requests
@@ -42,34 +43,56 @@ def transform_a_v_raw_data_to_df(data: dict, key_name: str) -> pd.DataFrame:
     return _rename_alpha_vantage_df_columns(df)
 
 
-def _check_imported_data(df: pd.DataFrame, ticker: str, data_type: str) -> None:
+def check_ohlc_df(
+    df: pd.DataFrame, data_type: str, volume_required: bool = False
+) -> None:
+    """
+    OHLC DataFrame should be non-empty,
+    contain all OHLC_REQUIRED_COLUMNS,
+    and all values should be numeric.
+    If it contains additional columns, it is OK.
+    """
+    OHLC_REQUIRED_COLUMNS = ["Close", "High", "Low", "Open"]
     if not isinstance(df, pd.DataFrame):
         raise ValueError(
-            f"In _check_imported_data ({ticker=}, {data_type=}): not instance of pd.DataFrame"
+            f"In check_ohlc_df ({data_type=}): not instance of pd.DataFrame"
         )
     if df.empty:
-        raise ValueError(
-            f"In _check_imported_data ({ticker=}, {data_type=}): empty DataFrame"
+        raise ValueError(f"In check_ohlc_df ({data_type=}): empty DataFrame")
+    for col_name in OHLC_REQUIRED_COLUMNS:
+        if col_name not in df.columns:
+            raise ValueError(
+                f"In check_ohlc_df ({data_type=}): column {col_name} is absent in DataFrame"
+            )
+    if volume_required:
+        if "Volume" not in df.columns:
+            raise ValueError(
+                f"In check_ohlc_df ({data_type=}): Volume column is absent in DataFrame"
+            )
+        all_columns_numeric = (
+            df[["Close", "High", "Low", "Open", "Volume"]]
+            .apply(lambda s: pd.to_numeric(s, errors="coerce").notnull().all())
+            .all()
         )
-    if not set(df.columns.values) == {"Volume", "Close", "High", "Low", "Open"}:
-        raise ValueError(
-            f"In _check_imported_data ({ticker=}, {data_type=}): wrong pd.DataFrame columns: {set(df.columns.values)}"
+    else:
+        all_columns_numeric = (
+            df[["Close", "High", "Low", "Open"]]
+            .apply(lambda s: pd.to_numeric(s, errors="coerce").notnull().all())
+            .all()
         )
-    all_columns_numeric = df.apply(
-        lambda s: pd.to_numeric(s, errors="coerce").notnull().all()
-    ).all()
     if not all_columns_numeric:
         raise ValueError(
-            f"In _check_imported_data ({ticker=}, {data_type=}): not all pd.DataFrame columns numeric"
+            f"In check_ohlc_df ({data_type=}): not all pd.DataFrame columns numeric"
         )
 
 
 def import_alpha_vantage_daily(ticker: str) -> pd.DataFrame:
+    """ """
     raw_data_daily: dict = get_daily_raw_from_alpha_vantage(ticker=ticker)
     data_daily: pd.DataFrame = transform_a_v_raw_data_to_df(
         data=raw_data_daily, key_name="Time Series (Daily)"
     )
-    _check_imported_data(df=data_daily, ticker=ticker, data_type="Daily")
+    check_ohlc_df(df=data_daily, data_type="Daily", volume_required=True)
     return data_daily
 
 
@@ -81,6 +104,10 @@ def import_ohlc_daily(ticker: str) -> pd.DataFrame:
     """
     internal_ticker = ticker.upper()
     local_file_path = _get_local_ticker_data_file_name(internal_ticker)
+    print(
+        f"Calling import_ohlc_daily with {internal_ticker=}, {local_file_path=}",
+        file=sys.stderr,
+    )
     if os.path.exists(local_file_path) and os.path.getsize(local_file_path) > 0:
         return pd.read_excel(local_file_path, index_col=0)
     res = import_alpha_vantage_daily(ticker=internal_ticker)
