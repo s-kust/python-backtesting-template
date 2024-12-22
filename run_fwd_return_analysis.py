6c820398efc83804a1f650ffe2cfdafc525ac72e
SergioKushchenko <serhii.kushchenko@appvales.com>
2024-12-22 20:29:30 +0100

    Added _check_feature_for_fwd_ret_days

--- run_fwd_return_analysis.py
+++ run_fwd_return_analysis.py
@@ -1,16 +1,103 @@
 # pylint: disable=C0121
 # pylint: disable=E2515
 import sys
+from typing import List
 
+import numpy as np
 import pandas as pd
 from dotenv import load_dotenv
 
-from constants import LOG_FILE, tickers_all
+from constants import (
+    DEFAULT_BOOTSTRAP_CONFIDENCE_LEVEL,
+    FEATURE_COL_NAME_BASIC,
+    LOG_FILE,
+    tickers_all,
+)
 from customizable.misc import get_ma_200_relation_label
 from features.f_v1_basic import add_features_v1_basic
 from utils.bootstrap import analyze_values_by_group, get_bootstrapped_mean_ci
 from utils.get_df_with_fwd_ret import add_fwd_ret
 from utils.local_data import TickersData
+
+
+def _check_feature_for_fwd_ret_days(
+    tickers_data: TickersData,
+    res: List[dict],
+    fwd_ret_days: int,
+    feature_col_name: str,
+    insert_empty_row: bool = True,
+) -> List[dict]:
+    """
+    Compare returns over fwd_ret_days subsequent days
+    in situations where the feature is True and False.
+    The function returns a list with dictionaries.
+    Later they will become rows in the final dataframe.
+    The function can also add an empty row
+    to make the dataframe easier to view.
+    To make this clear, see the dataframe example in the README.
+    """
+
+    for ticker in tickers_data.tickers_data_with_features:
+        if (
+            f"fwd_ret_{fwd_ret_days}"
+            not in tickers_data.tickers_data_with_features[ticker].columns
+        ):
+            tickers_data.tickers_data_with_features[ticker] = add_fwd_ret(
+                ohlc_df=tickers_data.tickers_data_with_features[ticker],
+                num_days=fwd_ret_days,
+            )
+
+    combined_ohlc_all = pd.DataFrame()
+    for ticker in tickers_data.tickers_data_with_features:
+        combined_ohlc_all = pd.concat(
+            [combined_ohlc_all, tickers_data.tickers_data_with_features[ticker]]
+        )
+
+    mask_feature_true = combined_ohlc_all[feature_col_name] == True
+    mask_feature_false = combined_ohlc_all[feature_col_name] == False
+
+    returns_f_true = (
+        combined_ohlc_all[mask_feature_true][f"fwd_ret_{fwd_ret_days}"].dropna().values
+    )
+
+    returns_f_false = (
+        combined_ohlc_all[mask_feature_false][f"fwd_ret_{fwd_ret_days}"].dropna().values
+    )
+
+    # This saves memory and speeds up the execution.
+    for ticker in tickers_data.tickers_data_with_features:
+        del tickers_data.tickers_data_with_features[ticker][f"fwd_ret_{fwd_ret_days}"]
+
+    res_f_true = get_bootstrapped_mean_ci(
+        data=returns_f_true, conf_level=DEFAULT_BOOTSTRAP_CONFIDENCE_LEVEL  # type: ignore
+    )
+    res_f_false = get_bootstrapped_mean_ci(
+        data=returns_f_false, conf_level=DEFAULT_BOOTSTRAP_CONFIDENCE_LEVEL  # type: ignore
+    )
+
+    # Adding columns fwd_ret_days and feature
+    # to later sort the dataframe and make it easy to view
+    res_f_true["fwd_ret_days"] = res_f_false["fwd_ret_days"] = fwd_ret_days
+    res_f_true["feature"] = True
+    res_f_false["feature"] = False
+
+    res.append(res_f_true)
+    res.append(res_f_false)
+
+    if insert_empty_row:
+        # create an empty row and append it to the resulting list
+        # to improve the viewing experience of the resulting DataFrame
+        res_f_empty = res_f_true.copy()
+        res_f_empty["feature"] = np.nan
+        res_f_empty[f"ci_left_{DEFAULT_BOOTSTRAP_CONFIDENCE_LEVEL}"] = np.nan
+        res_f_empty[f"ci_right_{DEFAULT_BOOTSTRAP_CONFIDENCE_LEVEL}"] = np.nan
+        res_f_empty["mean_val"] = np.nan
+        res_f_empty["positive_pct"] = np.nan
+        res_f_empty["count"] = np.nan
+        res.append(res_f_empty)
+
+    return res
+
 
 if __name__ == "__main__":
     load_dotenv()
@@ -32,6 +119,33 @@ if __name__ == "__main__":
     tickers_data = TickersData(
         tickers=tickers_all,
         add_feature_cols_func=add_features_v1_basic,
+    )
+
+    res: List[dict] = list()
+    FWD_RETURN_DAYS_MAX = 6
+    for fwd_return_days in range(2, FWD_RETURN_DAYS_MAX + 1):
+        print(
+            f"Now check for fwd returns {fwd_return_days} days - up to {FWD_RETURN_DAYS_MAX}"
+        )
+        res = _check_feature_for_fwd_ret_days(
+            tickers_data=tickers_data,
+            res=res,
+            fwd_ret_days=fwd_return_days,
+            insert_empty_row=True,
+            feature_col_name=FEATURE_COL_NAME_BASIC,
+        )
+    df = pd.DataFrame(res)
+
+    # change order of columns for convenience
+    df.insert(0, "feature", df.pop("feature"))
+    df.insert(0, "fwd_ret_days", df.pop("fwd_ret_days"))
+
+    df.sort_values(["fwd_ret_days", "feature"], ascending=[True, True])
+    df.loc[df["mean_val"] != df["mean_val"], "fwd_ret_days"] = np.nan
+    df.to_excel(EXCEL_FILE_NAME_SIMPLE, index=False)
+    print(
+        f"Analysis complete! Now you may explore the results file {EXCEL_FILE_NAME_SIMPLE}",
+        file=sys.stderr,
     )
 
     # Now add forward returns column fwd_ret_4 to analyze it
@@ -70,19 +184,6 @@ if __name__ == "__main__":
     # NOTE
     # in add_features_v1_basic()
     # res["feature"] = res["Close"] < res[f"ma_{MOVING_AVERAGE_N}"]
-
-    res = dict()
-    res["CLOSE_BELOW_MA_200"] = get_bootstrapped_mean_ci(
-        data=combined_ohlc_all[combined_ohlc_all["feature_basic"] == True]["fwd_ret_4"]
-        .dropna()
-        .values  # type: ignore
-    )
-    res["CLOSE_ABOVE_MA_200"] = get_bootstrapped_mean_ci(
-        data=combined_ohlc_all[combined_ohlc_all["feature_basic"] == False]["fwd_ret_4"]
-        .dropna()
-        .values  # type: ignore
-    )
-    pd.DataFrame(res).T.to_excel(EXCEL_FILE_NAME_SIMPLE)
 
     # advanced separation: by groups
 
