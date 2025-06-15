1618578a6418ade7af2d06dac35d30f4118b21b3
SergioKushchenko <serhii.kushchenko@appvales.com>
2025-06-15 09:43:30 +0200

    Split preliminary analysis of binary features and groups

--- run_fwd_return_analysis_binary.py
+++ run_fwd_return_analysis_binary.py
@@ -13,9 +13,9 @@ from constants import (
     LOG_FILE,
     tickers_all,
 )
-from customizable.misc import get_ma_200_relation_label
+from features.f_rsi import add_feature_high_rsi
 from features.f_v1_basic import add_features_v1_basic
-from utils.bootstrap import analyze_values_by_group, get_bootstrapped_mean_ci
+from utils.bootstrap import get_bootstrapped_mean_ci
 from utils.get_df_with_fwd_ret import add_fwd_ret
 from utils.local_data import TickersData
 
@@ -102,69 +102,12 @@ def _check_feature_for_fwd_ret_days(
     return res_to_return
 
 
-def add_group_label_analyze_save(
-    tickers_data: TickersData, excel_file_name: str, fwd_red_n_days: int
-) -> None:
-    group_col_name = "close_rel_ma_200_group"
-
-    # Now add forward returns column to analyze it
-
-    # NOTE We don't need forward returns to run backtests,
-    # so we add them only here,
-    # not inside the TickersData class or anywhere else.
-    for ticker in tickers_data.tickers_data_with_features:
-        tickers_data.tickers_data_with_features[ticker] = add_fwd_ret(
-            ohlc_df=tickers_data.tickers_data_with_features[ticker],
-            num_days=fwd_red_n_days,
-        )
-
-    # Add a column with a group label
-    # and concatenate the DFs of all tickers into one large DF.
-    combined_ohlc_all = pd.DataFrame()
-    for ticker, ohlc_df in tickers_data.tickers_data_with_features.items():
-        ohlc_df[group_col_name] = ohlc_df.apply(get_ma_200_relation_label, axis=1)
-        combined_ohlc_all = pd.concat([combined_ohlc_all, ohlc_df])
-        del ohlc_df[group_col_name]
-        del ohlc_df[f"fwd_ret_{fwd_red_n_days}"]
-
-    combined_ohlc_all = combined_ohlc_all.dropna()
-
-    # just in case...
-    print(f"{combined_ohlc_all.shape=}")
-    print(f"{combined_ohlc_all.columns=}")
-    print(combined_ohlc_all.tail())
-
-    # Up until this point there has been preparation,
-    # and now the analysis will be carried out.
-
-    # NOTE This is for convenient sorting of rows
-    # in the resulting Excel file.
-    group_order_ma_200_rel = {
-        "HIGHLY_ABOVE": 1,
-        "MODERATELY_ABOVE": 2,
-        "SLIGHTLY_ABOVE": 3,
-        "SLIGHTLY_BELOW": 4,
-        "MODERATELY_BELOW": 5,
-        "HIGHLY_BELOW": 6,
-        "all_data": 7,  # all_data row is important, don't miss it
-    }
-
-    analyze_values_by_group(
-        df=combined_ohlc_all,
-        group_col_name=group_col_name,
-        values_col_name=f"fwd_ret_{fwd_red_n_days}",
-        group_order_map=group_order_ma_200_rel,
-        excel_file_name=excel_file_name,
-    )
-
-
 if __name__ == "__main__":
     load_dotenv()
 
     # clear LOG_FILE every time
     open(LOG_FILE, "w", encoding="UTF-8").close()
 
-    EXCEL_FILE_NAME_BY_GROUP = "res_ma_200_by_group.xlsx"
     EXCEL_FILE_NAME_SIMPLE = "res_ma_200_above_below.xlsx"
 
     # The first step is to collect DataFrames with data and derived columns
@@ -175,8 +118,8 @@ if __name__ == "__main__":
     # For more details, see the class TickersData internals
     # and the add_features_v1_basic function.
     tickers_data_instance = TickersData(
-        tickers=tickers_all,
-        add_feature_cols_func=add_features_v1_basic,
+        tickers=["SLV"],
+        add_feature_cols_func=add_feature_high_rsi,
     )
 
     # Now we run the test for each number of days in the range,
@@ -197,7 +140,7 @@ if __name__ == "__main__":
         )
     df = pd.DataFrame(res)
 
-    # Manipulate with columns for viewing convenience
+    # Manipulate with columns order for viewing convenience
     df.insert(0, "feature", df.pop("feature"))
     df.insert(0, "fwd_ret_days", df.pop("fwd_ret_days"))
     df.sort_values(["fwd_ret_days", "feature"], ascending=[True, True])
@@ -206,18 +149,5 @@ if __name__ == "__main__":
     df.to_excel(EXCEL_FILE_NAME_SIMPLE, index=False)
     print(
         f"Analysis complete! Now you may explore the results file {EXCEL_FILE_NAME_SIMPLE}",
-        file=sys.stderr,
-    )
-
-    # NOTE It is not obligatory to bother with splitting into groups
-    # and calling add_group_label_analyze_save().
-    # The method demonstrated above may be enough for you.
-    add_group_label_analyze_save(
-        tickers_data=tickers_data_instance,
-        excel_file_name=EXCEL_FILE_NAME_BY_GROUP,
-        fwd_red_n_days=4,
-    )
-    print(
-        f"Complete! Please see the files {EXCEL_FILE_NAME_SIMPLE}, {EXCEL_FILE_NAME_BY_GROUP}",
         file=sys.stderr,
     )
---
+++ run_fwd_return_analysis_groups.py
@@ -0,0 +1,121 @@
+import sys
+from typing import Callable
+
+import pandas as pd
+from dotenv import load_dotenv
+
+from constants import LOG_FILE
+from features.f_rsi import add_feature_high_rsi
+from features.partition_groups.rsi_bounds import (
+    get_rsi_group_label,
+    group_order_rsi_bounds,
+)
+from utils.bootstrap import analyze_values_by_group
+from utils.get_df_with_fwd_ret import add_fwd_ret
+from utils.local_data import TickersData
+
+GROUP_COLUMN_NAME = "group_label"
+
+
+def _get_combined_df_with_fwd_ret(
+    tickers_data: TickersData, fwd_red_n_days: int, labelling_func: Callable
+) -> pd.DataFrame:
+    """
+    Prepare a combined dataframe with a column containing the group label
+    for all tickers contained in tickers_data.
+    """
+
+    # Now add forward returns column to analyze it
+    # NOTE We don't need forward returns to run backtests,
+    # so we add them only here,
+    # not inside the TickersData class or anywhere else.
+    for ticker in tickers_data.tickers_data_with_features:
+        tickers_data.tickers_data_with_features[ticker] = add_fwd_ret(
+            ohlc_df=tickers_data.tickers_data_with_features[ticker],
+            num_days=fwd_red_n_days,
+        )
+
+    # Add a column with a group label
+    # and concatenate the DFs of all tickers into one large DF.
+    combined_ohlc_all = pd.DataFrame()
+    for ticker, ohlc_df in tickers_data.tickers_data_with_features.items():
+        ohlc_df[GROUP_COLUMN_NAME] = ohlc_df.apply(labelling_func, axis=1)
+        combined_ohlc_all = pd.concat([combined_ohlc_all, ohlc_df])
+        del ohlc_df[GROUP_COLUMN_NAME]
+        del ohlc_df[f"fwd_ret_{fwd_red_n_days}"]
+
+    combined_ohlc_all = combined_ohlc_all.dropna()
+
+    # just in case...
+    # print(f"{combined_ohlc_all.shape=}")
+    # print(f"{combined_ohlc_all.columns=}")
+    print(combined_ohlc_all.tail())
+
+    return combined_ohlc_all
+
+
+def _filter_df_by_date(
+    df: pd.DataFrame, date_threshold: str, remaining_part: str
+) -> pd.DataFrame:
+    if remaining_part not in ["above", "below"]:
+        raise ValueError(
+            f"_filter_df_by_date: {remaining_part=}, should be above or below"
+        )
+    if remaining_part == "above":
+        return df.loc[df.index >= date_threshold]
+    return df.loc[df.index < date_threshold]
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    # clear LOG_FILE every time
+    open(LOG_FILE, "w", encoding="UTF-8").close()
+
+    # The first step is to collect DataFrames with data and derived columns
+    # for all the tickers we are interested in.
+    # This data is stored in the TickersData class instance
+    # as a dictionary whose keys are tickers and values ​are DFs.
+
+    # For more details, see the class TickersData internals
+    # and the add_features_v1_basic function.
+
+    # TODO Get rid of adding a binary feature if we don't need it.
+    # Now inside the TickersData class a binary feature will be added
+    # only for adding the RSI column and then assign a group based on RSI value.
+    tickers_data_instance = TickersData(
+        tickers=["GLD"],
+        add_feature_cols_func=add_feature_high_rsi,
+    )
+
+    for fwd_ret_days in range(13, 14):
+
+        combined_ohlc_group_df = _get_combined_df_with_fwd_ret(
+            tickers_data=tickers_data_instance,
+            fwd_red_n_days=fwd_ret_days,
+            labelling_func=get_rsi_group_label,
+        )
+
+        RES_FILE_NAME = f"res_GLD_by_RSI_group_{fwd_ret_days}_days.xlsx"
+
+        # NOTE With this function, you can analyze only data for the latest periods.
+        # Or, conversely, analyze older data, excluding recent periods
+        # from consideration.
+
+        # combined_ohlc_group_df = _filter_df_by_date(
+        #     df=combined_ohlc_group_df,
+        #     date_threshold="2023-01-01",
+        #     remaining_part="below",
+        # )
+
+        analyze_values_by_group(
+            df=combined_ohlc_group_df,
+            group_col_name=GROUP_COLUMN_NAME,
+            values_col_name=f"fwd_ret_{fwd_ret_days}",
+            group_order_map=group_order_rsi_bounds,
+            excel_file_name=RES_FILE_NAME,
+        )
+        print(
+            f"Complete! Please see the file {RES_FILE_NAME}",
+            file=sys.stderr,
+        )
