0f41f416b067fe69fb3211b418b83296e3647082
SergioKushchenko <serhii.kushchenko@appvales.com>
2025-11-03 05:35:28 +0100

    Added function check_initial_balance_breach and tests

--- derivative_columns/initial_balance.py
+++ derivative_columns/initial_balance.py
@@ -63,3 +63,69 @@ def add_col_ib_high_low(
 
     # 3. Return the dataframe
     return df
+
+
+def check_initial_balance_breach(
+    df: pd.DataFrame, initial_balance_minutes: int = 30
+) -> pd.DataFrame:
+    """
+    Checks for the first Initial Balance Low Breakdown (ib_low_bd) and High Breakout (ib_high_bt)
+    after the initial balance period for each day. Uss the Close price for the breach check.
+
+    Args:
+        df: A Pandas DataFrame with 'ib_low' and 'ib_high' columns.
+        initial_balance_minutes: The duration of the initial balance period
+                                 in minutes (15, 30, 45, or 60).
+
+    Returns:
+        The modified DataFrame with 'ib_low_bd' and 'ib_high_bt' Boolean columns.
+    """
+
+    # 1. Check input validity
+    if initial_balance_minutes not in [15, 30, 45, 60]:
+        raise ValueError(
+            "check_initial_balance_breach: Initial balance must be 15, 30, 45, or 60 minutes."
+        )
+
+    check_df_res = check_df_format(df=df)
+    if check_df_res["is_datetime_index"] is False:
+        raise ValueError(
+            f"check_initial_balance_breach: input DF doesn't have DateTime index, {check_df_res}"
+        )
+    if check_df_res["inferred_interval_minutes"] not in [5, 15, 30, 45, 60]:
+        inferred_interval_min = check_df_res["inferred_interval_minutes"]
+        raise ValueError(
+            f"check_initial_balance_breach: input DF bars interval = {inferred_interval_min} min, should be 5, 15, 30, 45, 60 minutes"
+        )
+
+    # 2. Create new Boolean columns
+    df["ib_low_bd"] = False
+    df["ib_high_bt"] = False
+
+    # Calculate number of bars in the initial balance period
+    num_bars = initial_balance_minutes // check_df_res["inferred_interval_minutes"]
+    bar_number_in_day = df.groupby(df.index.date).cumcount()  # type: ignore
+    post_ib_mask = bar_number_in_day >= num_bars
+
+    # 3. Determine breakdown and breakout using 'Close' price
+
+    breakdown_candidates = (df["Close"] < df["ib_low"]) & post_ib_mask
+
+    first_breakdown_indices = (
+        breakdown_candidates[breakdown_candidates]
+        .groupby(breakdown_candidates[breakdown_candidates].index.date)
+        .idxmin()
+    )
+    df.loc[first_breakdown_indices, "ib_low_bd"] = True
+
+    breakout_candidates = (df["Close"] > df["ib_high"]) & post_ib_mask
+
+    first_breakout_indices = (
+        breakout_candidates[breakout_candidates]
+        .groupby(breakout_candidates[breakout_candidates].index.date)
+        .idxmin()
+    )
+    df.loc[first_breakout_indices, "ib_high_bt"] = True
+
+    # 4. Return the modified dataframe
+    return df
---
+++ tests/ib_low_high/test_check_initial_balance_breach.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import pytest
+
+from derivative_columns.initial_balance import check_initial_balance_breach
+
+
+def test_check_initial_balance_breach_ok(df_5_min_with_ib: pd.DataFrame) -> None:
+    res = check_initial_balance_breach(df=df_5_min_with_ib)
+    assert "ib_high_bt" in res.columns
+    assert "ib_low_bd" in res.columns
+    res.to_csv("./df_5m_with_IB_bd_bt.csv", encoding="utf-8", header=True, index=True)
+
+
+def test_check_initial_balance_breach_no_datetime_index_in_df(
+    empty_df: pd.DataFrame,
+) -> None:
+    """
+    Run check_initial_balance_breach with empty DF as input.
+    """
+    with pytest.raises(ValueError) as exc_info:
+        check_initial_balance_breach(df=empty_df)
+    exc_info_value = str(exc_info.value)
+    assert "input DF doesn't have DateTime index" in exc_info_value
+    assert "'inferred_interval_minutes': 999" in exc_info_value
+
+
+def test_check_initial_balance_breach_daily_df(spy_df_daily: pd.DataFrame) -> None:
+    """
+    Run check_initial_balance_breach with daily OHLC DF as input.
+    It has wrong bars interval 1440 min, i.e. 24 h.
+    """
+    with pytest.raises(ValueError) as exc_info:
+        check_initial_balance_breach(df=spy_df_daily)
+    exc_info_value = str(exc_info.value)
+    assert (
+        "check_initial_balance_breach: input DF bars interval = 1440 min, should be 5, 15, 30, 45, 60 minutes"
+        in exc_info_value
+    )
